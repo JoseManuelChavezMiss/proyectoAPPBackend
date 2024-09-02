b24c3fb4f6d008679a26f5b461dce521d5df4091
TuNombre <jchavezm11@miumg.edu.gt>
2024-09-02 11:37:24 -0600

    PRUEBAS SERVIDOR

--- src/main/java/proyectoAPPBackend/proyectoAPPBackend/security/controller/AuthController.java
+++ src/main/java/proyectoAPPBackend/proyectoAPPBackend/security/controller/AuthController.java
@@ -16,7 +16,7 @@ import java.text.ParseException;
 @RestController
 @RequestMapping("/auth")
 //@CrossOrigin( origins = "http://localhost:4200/")
-@CrossOrigin( origins = "http://aguasanta.store/")
+@CrossOrigin( origins = "https://aguasanta.store/")
 public class AuthController {
 
     @Autowired
